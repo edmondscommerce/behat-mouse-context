b034efdeab6a1814907a32961bd52d240c52722a
Kenneth Bradley-White <ken@edmondscommerce.co.uk>
2016-02-02 10:19:51 +0000

    Added events for hover, right-click and double-click

--- src/MouseContext.php
+++ src/MouseContext.php
@@ -8,6 +8,54 @@ use Behat\Behat\Context\SnippetAcceptingContext;
 
 class MouseContext extends RawMinkContext implements Context, SnippetAcceptingContext {
 
+    /**
+     * @When /^I hover over the element "([^"]*)"$/
+     */
+    public function iHoverOverTheElement($locator)
+    {
+        $session = $this->getSession(); // get the mink session
+        $element = $session->getPage()->find('css', $locator); // runs the actual query and returns the element
+
+        // errors must not pass silently
+        if (null === $element) {
+            throw new \InvalidArgumentException(sprintf('Could not evaluate CSS selector: "%s"', $locator));
+        }
+
+        $element->mouseOver();
+    }
+
+    /**
+     * @When /^I right-click the element "([^"]*)"$/
+     */
+    public function iRightClickTheElement($locator)
+    {
+        $session = $this->getSession(); // get the mink session
+        $element = $session->getPage()->find('css', $locator); // runs the actual query and returns the element
+
+        // errors must not pass silently
+        if (null === $element) {
+            throw new \InvalidArgumentException(sprintf('Could not evaluate CSS selector: "%s"', $locator));
+        }
+
+        $element->rightClick();
+    }
+
+    /**
+     * @When /^I double-click the element "([^"]*)"$/
+     */
+    public function iDoubleClickOverTheElement($locator)
+    {
+        $session = $this->getSession(); // get the mink session
+        $element = $session->getPage()->find('css', $locator); // runs the actual query and returns the element
+
+        // errors must not pass silently
+        if (null === $element) {
+            throw new \InvalidArgumentException(sprintf('Could not evaluate CSS selector: "%s"', $locator));
+        }
+
+        $element->doubleClick();
+    }
+
 
 
 }
